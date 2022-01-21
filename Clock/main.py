4809b7e78314de0f0ef02cfb33fac40ab398d9f0
Eraldo Neto <Eraldoosantos@gmail.com>
2022-01-20 21:59:58 -0300

    Add files via upload

---
+++ Clock/main.py
@@ -0,0 +1,23 @@
+# Tkinker is a common GUI to provide interfaces
+# tkinker.tkk allows us tu use widgets
+
+from tkinter import *
+from tkinter.ttk import * 
+from time import strftime
+
+# GUI module 
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    time_string = strftime('%H:%M:%S %p')
+    label.config(text=time_string)
+    label.after(1000, time)
+
+# A lable to store the clock
+label = Label(root, font=("ds-digital", 80), background="black", foreground="cyan")
+label.pack(anchor="center")
+time()
+
+mainloop()
